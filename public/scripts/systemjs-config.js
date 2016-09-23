c60cf466b6dde053b69908bcf99cf95f4d746899
Nikolay <nikolayprodanow@gmail.com>
2016-09-23 17:51:21 +0300

    Add html and some setup of app

---
+++ public/scripts/systemjs-config.js
@@ -0,0 +1,12 @@
+(function () {
+    SystemJS.config({
+        transpiler: 'plugin-babel',
+        map:{
+            'plugin-babel': '../node_modules/systemjs-plugin-babel/plugin-babel.js',
+            'systemjs-babel-build': '../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+            'main': './scripts/main.js'
+        }
+    });
+
+    System.import('main');
+})();
