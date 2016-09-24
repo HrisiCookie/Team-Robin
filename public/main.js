46565e0c1db464407ec2140477d4999724c16c24
Nikolay <nikolayprodanow@gmail.com>
2016-09-24 10:41:20 +0300

    Add example Navigation

--- public/main.js
+++ public/main.js
@@ -2,15 +2,18 @@ import { homeController } from './scripts/controllers/home-controller.js';
 import { usersController } from './scripts/controllers/users-controller.js';
 
 (function () {
-    let sammyApp = Sammy('#content', function(){
+    let sammyApp = Sammy('#content', function () {
 
-        this.get('#/', homeController.all);
+        this.get('#/', function () {
+            this.redirect('#/home');
+        });
+        this.get('#/home', homeController.all);
         this.get('#/login', usersController.login);
         this.get('#/register', usersController.register);
-        
+
     });
 
-    $(function(){
+    $(function () {
         sammyApp.run('#/');
     });
 })();
